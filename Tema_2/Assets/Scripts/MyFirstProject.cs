64abc550f41eb4923069138e2234069e555a037b
Marc553 <91455235+Marc553@users.noreply.github.com>
2021-10-06 14:02:37 +0200

    primerprojectoT2

---
+++ Tema_2/Assets/Scripts/MyFirstProject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstProject : MonoBehaviour
+{
+    public int playerAge = 35;
+    private float playerSpeed = 5.25f;
+    public string playerName = "Marc";
+    [SerializeField] private bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
