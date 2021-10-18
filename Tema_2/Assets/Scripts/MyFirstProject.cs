f0694524c964f1b965552a0c63be9f5a75a3e7b9
Marc553 <91455235+Marc553@users.noreply.github.com>
2021-10-18 10:48:43 +0200

    clase 3 de programacion

--- Tema_2/Assets/Scripts/MyFirstProject.cs
+++ Tema_2/Assets/Scripts/MyFirstProject.cs
@@ -3,16 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MyFirstProject : MonoBehaviour
-{
-    public int playerAge = 35;
+{/*
+    
     private float playerSpeed = 5.25f;
     public string playerName = "Marc";
     [SerializeField] private bool gameOver;
+    
+    public int x = 5;
+    public int y = 5;
 
-    // Start is called before the first frame update
+    public float a = 5f;
+    public float b = 2f;
+
+    public bool isRaining = false;
+    // Start is called before the first frame update*/
+    public int playerAge = 35;
+
     void Start()
-    {
+    {/*
+        Debug.Log($"Suma: {x} + {y} = {x + y}");
+        Debug.Log("Resta: " + x + " - " + y + " = " + (x - y));
+        Debug.Log("Multiplicacion: " + x + " * " + y + " = " + (x * y));
+        Debug.Log("Division: " + x + " / " + y + " = " + (x / y));
+        Debug.Log("Division: " + x + " / " + y + " = " + (a / b));
+        Debug.Log(string.Format("Producto: {0} * {1} = {2}", x, y, x * y));
+        Debug.Log(string.Format("Division: {0} / {1} = {2}", x, y, x / y));
         
+        if (isRaining == true)
+        {
+            Debug.Log("Lleva paraguas.");
+        }
+        else
+        {
+            Debug.Log("No llueve, no lleves paraguas.");
+        }
+*/
+         if(playerAge >= 18)
+        {
+            Debug.Log("Eres mayor de edad.");
+        }
+         else if (playerAge <= 13)
+        {
+            Debug.Log("Eres adolescente.");
+        }
+         else
+        {
+            Debug.Log("Eres un niño.");
+        }
     }
 
     // Update is called once per frame
--- Tema_2/Assets/Scripts/Vehicle.cs
+++ Tema_2/Assets/Scripts/Vehicle.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
+    // Variable que gurda el sonido del vehiculo.
+    [SerializeField] private string sound = "!BRUM BRUM!";
+    [SerializeField] private string name = "Coche";
+    [SerializeField] private int numberWheels = 4;
+    //La variable name guarda el nombre del vehiculo y la variable numberWheels guarda el numero de ruedas del vehiculo.
+    [SerializeField] private bool hasSirena = true;
+    //Comprueba si "Ambulancia" tiene sirena.
+    [SerializeField] private bool isOn = true;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
+    {/*
+        Debug.Log($"{name} tiene {numberWheels} ruedas y hace {sound}");
+        if (hasSirena) //Cuando sea true dira que ambulancia tiene sirena
+        {
+            Debug.Log($"{name} tiene sirena.");
+        }
+        else
+        {
+            Debug.Log($"{name} no tiene sirena.");
+        }*/
+        if (isOn)
+        {
+            Debug.Log($"{sound}");
+        }
+        else if (hasSirena)
+        {
+            Debug.Log($"{name} hará {sound} cuando se ponga en marcha");
+        }
+        else
+        {
+            Debug.Log($"{name} no está en marcha.");
 
+        }
+    }
     // Update is called once per frame
     void Update()
     {
